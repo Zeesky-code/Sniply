4f2c3e3c072fc597cbf8abf5d5d0af55bd566df3
Zainab Lawal <zainablaw1012@gmail.com>
2023-06-17 13:04:57 +0300

    Switch to typescript

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
-
+    res.send('Welcome to Sniply!');
+});
 app.listen(port, () => {
     console.log(`Sniply listening at http://localhost:${port}`);
-    }
-);
+});
