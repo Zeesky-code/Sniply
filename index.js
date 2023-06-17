8be9ba811c8aa9b0b35e103ab356b9e3cabe54be
Zainab Lawal <zainablaw1012@gmail.com>
2023-06-17 12:06:27 +0300

    Project Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Sniply listening at http://localhost:${port}`);
+    }
+);
