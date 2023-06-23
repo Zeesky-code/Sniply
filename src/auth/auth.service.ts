f2e2f81ef9b9e707fabfb10023001b20872d13c6
Zainab Lawal <zainablaw1012@gmail.com>
2023-06-23 13:34:46 +0300

    set up user auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, UnauthorizedException, ConflictException } from '@nestjs/common';
+import {UsersService} from '../users/users.service';
+impoer
+
+@Injectable()
+export class AuthService {}
