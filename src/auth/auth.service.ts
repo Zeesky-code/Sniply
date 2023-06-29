8c715508c4e496cf4b339825f443c232ff13df6f
Zainab Lawal <zainablaw1012@gmail.com>
2023-06-29 12:54:11 +0300

    switch  main  branch to frontend

--- src/auth/auth.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable, UnauthorizedException, ConflictException } from '@nestjs/common';
-import {UsersService} from '../users/users.service';
-impoer
-
-@Injectable()
-export class AuthService {}
--- src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  exports: [UsersService],
-  providers: [PrismaService, UsersService],
-})
-export class UsersModule {}
