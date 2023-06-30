dfd274279ff4d608a4dea6f5af047b3133e7bdaa
Zainab Lawal <zainablaw1012@gmail.com>
2023-06-30 17:34:11 +0300

    Added Signup logic

--- src/routes/signup.jsx
+++ src/routes/signup.jsx
@@ -1,74 +1,98 @@
-import { Form } from "react-router-dom";
 import { useState } from 'react';
 
+
 export default function SignupPage() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
-    // States for checking the errors
-    const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
 
-    // Handling the name change
-    const handleUsername = (e) => {
-        setUsername(e.target.value);
-        setSubmitted(false);
-    };
+  // Handling the name change
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+    setSubmitted(false);
+  };
 
-    // Handling the email change
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        setSubmitted(false);
-    };
+  // Handling the email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
 
-    // Handling the password change
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-        setSubmitted(false);
-    };
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
 
-    const handleSignup = async (e) => {
-        e.preventDefault();
-        try {
-          let res = await fetch("https://sniply.onrender.com/auth/signup", {
-            method: "POST",
-            body: JSON.stringify({
-              username: username,
-              email: email,
-              password: password,
-            }),
-          });
-          let resJson = await res.json();
-          if (res.status === 201) {
-            setUsername("");
-            setEmail("");
-            setPassword("");
-            setMessage("User created successfully");
-          } else {
-            setMessage(res.json.message);
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      };
-    return (
-        <div>
-            <h1>Signup Page</h1>
-            <Form action="edit">
-                <label className="label">Username</label>
-                <input onChange={handleUsername} className="input" value={username} type="text" />
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(username + ' ' + email + ' ' + password);
+      let res = await fetch('/auth/signup', {
+        method: 'POST',
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        })
+      });
+      let resJson = await res.json();
+      if (res.status === 201) {
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setMessage('User created successfully');
+        window.location = '/login'
+      } else {
+        setMessage(resJson.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-                <label className="label">Email</label>
-                <input onChange={handleEmail} className="input" value={email} type="email" />
+  return (
+    <form>
+      <h1>Signup Page</h1>
+      <div className="message">{message ? <p>{message}</p> : null}</div>
+      <div>
+        <label className="label">Username</label>
+        <input
+          onChange={handleUsername}
+          className="input"
+          value={username}
+          type="text"
+        />
 
-                <label className="label">Password</label>
-                <input onChange={handlePassword} className="input" value={password} type="password" />
-                <button type="submit" onClick={handleSignup}>Sign Up</button>
-            </Form>
-            <div className="message">{message ? <p>{message}</p> : null}</div>
-        </div>
+        <label className="label">Email</label>
+        <input
+          onChange={handleEmail}
+          className="input"
+          value={email}
+          type="email"
+        />
 
-    )
+        <label className="label">Password</label>
+        <input
+          onChange={handlePassword}
+          className="input"
+          value={password}
+          type="password"
+        />
+        <button type="submit" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </div>
+
+    </form>
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth/signup': {
+        target: 'https://sniply.onrender.com',
+        changeOrigin: true,
+      },
+    },
+  },
 })
