3c6506cf65bbea67aa1033d42a16ee493fc302bd
Zainab Lawal <zainablaw1012@gmail.com>
2023-07-21 11:48:08 +0300

    remove unnecessary console logs

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -24,7 +24,6 @@ export default function LoginPage() {
     };
 
     const handleLogin = async (e) => {
-        console.log(email + " " + password)
         e.preventDefault();
         try {
             const response = await fetch('/auth/login', {
@@ -39,7 +38,6 @@ export default function LoginPage() {
                 }),
             });
             const data = await response.json();
-            console.log(data)
             if (response.status === 200) {
                 const token = data.data.token;
                 localStorage.setItem('token', token);
